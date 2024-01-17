c952c19cd17680a3bd6dee9b6cd762b239cca434
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-01-17 20:59:34 +0100

    added lists of current and future skills

---
+++ src/skillset.js
@@ -0,0 +1,32 @@
+export const mySkills = [
+    "Semantic & accessible HTML",
+    "React",
+    "Promises, Async/Await",
+    "Responsive Web Design",
+    "React Router",
+    "GitHub Pull Requests & Review",
+    // eslint-disable-next-line
+    "JavaScript: ES6+",
+    "React Hooks",
+    "Trello",
+    "Markdown",
+    "Redux (Toolkit)",
+    "Scrum",
+    "Immutability",
+    "Error handling",
+    "NPM",
+    "CSS BEM convention",
+    "Working with API (fetch, axios)",
+    "Teamwork",
+    "CSS Grid",
+    "Redux-Saga",
+    "Git",
+    "CSS Flexbox",
+];
+
+export const myFutureSkills = [
+    "TypeScript",
+    "Node.js",
+    "React Context",
+    "JS classes",
+];
