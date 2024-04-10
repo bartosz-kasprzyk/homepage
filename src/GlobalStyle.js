c5e179ff608734675a7289468325a10ce3011b29
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-04-10 20:09:16 +0200

    removed old font-family

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     html {
         box-sizing: border-box;
-        font-family: 'Montserrat', sans-serif;
         font-size: 15px;
         line-height: 1.5;
     }
