a3796c0bc620c0e2847cb54d221591110c1d4155
bartek-kasprzyk <bartosz.kasprzyk58@gmail.com>
2024-05-10 16:56:39 +0200

    added scroll to top on refresh

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { GlobalStyle } from "./GlobalStyle";
 import { darkTheme, lightTheme } from "./theme";
 import { selectIsDarkMode } from './components/Header/Switch/themeSlice';
 import PersonalHomepage from './features/Homepage';
+import { useEffect } from 'react';
 
 function App() {
   const isDarkMode = useSelector(selectIsDarkMode)
+
+  useEffect(() => {
+    window.history.scrollRestoration = 'manual'
+  }, []);
+
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <GlobalStyle />
